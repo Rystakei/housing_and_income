d3039276b2fd49f83f6cfbd261a69da54ecc17db
Krista Williams <rystakei@gmail.com>
2015-05-11 06:01:12 -0400

    wip

---
+++ app/scripts/parse_historic_data.rb
@@ -0,0 +1,66 @@
+year = 1967
+
+data = []
+
+stop_year = 2012
+
+while year <= 2012
+	data << {"year" => year, "income" => 42934}
+	year += 1
+end
+
+puts data
+
+historical_prices = [{" Period": 1963," Median ":" $18,000"," Average":" $19,300"},
+{" Period": 1964," Median ":" $18,900"," Average":" $20,500"},
+{" Period": 1965," Median ":" $20,000"," Average":" $21,500"},
+{" Period": 1966," Median ":" $21,400"," Average":" $23,300"},
+{" Period": 1967," Median ":" $22,700"," Average":" $24,600"},
+{" Period": 1968," Median ":" $24,700"," Average":" $26,600"},
+{" Period": 1969," Median ":" $25,600"," Average":" $27,900"},
+{" Period": 1970," Median ":" $23,400"," Average":" $26,600"},
+{" Period": 1971," Median ":" $25,200"," Average":" $28,300"},
+{" Period": 1972," Median ":" $27,600"," Average":" $30,500"},
+{" Period": 1973," Median ":" $32,500"," Average":" $35,500"},
+{" Period": 1974," Median ":" $35,900"," Average":" $38,900"},
+{" Period": 1975," Median ":" $39,300"," Average":" $42,600"},
+{" Period": 1976," Median ":" $44,200"," Average":" $48,000"},
+{" Period": 1977," Median ":" $48,800"," Average":" $54,200"},
+{" Period": 1978," Median ":" $55,700"," Average":" $62,500"},
+{" Period": 1979," Median ":" $62,900"," Average":" $71,800"},
+{" Period": 1980," Median ":" $64,600"," Average":" $76,400"},
+{" Period": 1981," Median ":" $68,900"," Average":" $83,000"},
+{" Period": 1982," Median ":" $69,300"," Average":" $83,900"},
+{" Period": 1983," Median ":" $75,300"," Average":" $89,800"},
+{" Period": 1984," Median ":" $79,900"," Average":" $97,600"},
+{" Period": 1985," Median ":" $84,300"," Average":" $100,800"},
+{" Period": 1986," Median ":" $92,000"," Average":" $111,900"},
+{" Period": 1987," Median ":" $104,500"," Average":" $127,200"},
+{" Period": 1988," Median ":" $112,500"," Average":" $138,300"},
+{" Period": 1989," Median ":" $120,000"," Average":" $148,800"},
+{" Period": 1990," Median ":" $122,900"," Average":" $149,800"},
+{" Period": 1991," Median ":" $120,000"," Average":" $147,200"},
+{" Period": 1992," Median ":" $121,500"," Average":" $144,100"},
+{" Period": 1993," Median ":" $126,500"," Average":" $147,700"},
+{" Period": 1994," Median ":" $130,000"," Average":" $154,500"},
+{" Period": 1995," Median ":" $133,900"," Average":" $158,700"},
+{" Period": 1996," Median ":" $140,000"," Average":" $166,400"},
+{" Period": 1997," Median ":" $146,000"," Average":" $176,200"},
+{" Period": 1998," Median ":" $152,500"," Average":" $181,900"},
+{" Period": 1999," Median ":" $161,000"," Average":" $195,600"},
+{" Period": 2000," Median ":" $169,000"," Average":" $207,000"},
+{" Period": 2001," Median ":" $175,200"," Average":" $213,200"},
+{" Period": 2002," Median ":" $187,600"," Average":" $228,700"},
+{" Period": 2003," Median ":" $195,000"," Average":" $246,300"},
+{" Period": 2004," Median ":" $221,000"," Average":" $274,500"},
+{" Period": 2005," Median ":" $240,900"," Average":" $297,000"},
+{" Period": 2006," Median ":" $246,500"," Average":" $305,900"},
+{" Period": 2007," Median ":" $247,900"," Average":" $313,600"},
+{" Period": 2008," Median ":" $232,100"," Average":" $292,600"},
+{" Period": 2009," Median ":" $216,700"," Average":" $270,900"},
+{" Period": 2010," Median ":" $221,800"," Average":" $272,900"},
+{" Period": 2011," Median ":" $227,200"," Average":" $267,900"},
+{" Period": 2012," Median ":" $245,200"," Average":" $292,200"},
+{" Period": 2013," Median ":" $268,900"," Average":" $324,500"},
+{" Period": 2014," Median ":" $282,800"," Average":" $345,800"},
+{" Period": ," Median ":" "," Average":" "}]
