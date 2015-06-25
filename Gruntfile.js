0ddadd7c8bd9e5c074bf7345e41d075b1410b21d
Krista Williams <rystakei@gmail.com>
2015-06-24 23:53:30 -0400

    Added data json file

--- Gruntfile.js
+++ Gruntfile.js
@@ -207,7 +207,7 @@ module.exports = function (grunt) {
       dist: {
         files: {
           src: [
-            '<%= config.dist %>/scripts/{,*/}*.js',
+            '<%= config.dist %>/scripts/{,*/}*.{js,json}',
             '<%= config.dist %>/styles/{,*/}*.css',
             '<%= config.dist %>/images/{,*/}*.*',
             '<%= config.dist %>/styles/fonts/{,*/}*.*',
